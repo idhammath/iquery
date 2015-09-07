87994d997ba0ad7b138d35bf1f867378c3898270
idhammath <idham.math@gmail.com>
2015-09-07 11:55:17 +0700

    Create sitemapData.js

---
+++ js/sitemapData.js
@@ -0,0 +1,252 @@
+var sitemapData = [{
+    name: 'Interface Query',
+    href: sitemapHrefBase + '../index.html',
+    nodes: [{
+        name: 'Demos',
+        href: sitemapHrefBase + '../demos/index.html',
+        nodes: [{
+            name: 'Behaviours',
+            href: sitemapHrefBase + 'behaviours/index.html',
+            nodes: [
+                {
+                    name: 'Forward Mouse Events',
+                    href: sitemapHrefBase + 'behaviours/forward-mouse-events/index.html'
+                },
+                {
+                    name: 'Movable',
+                    href: sitemapHrefBase + 'behaviours/movable/index.html',
+                    nodes: [
+                        {
+                            name: 'Multi Movable',
+                            href: sitemapHrefBase + 'behaviours/movable/multi-move.html'
+                        },
+                        {
+                            name: 'Dom Movable',
+                            href: sitemapHrefBase + 'behaviours/movable/dom-move.html'
+                        }
+                    ]
+                },
+                {
+                    name: 'No Select',
+                    href: sitemapHrefBase + 'behaviours/no-select/index.html'
+                },
+                {
+                    name: 'Resizable',
+                    href: sitemapHrefBase + 'behaviours/resizable/index.html'
+                },
+                {
+                    name: 'Restrict Text',
+                    href: sitemapHrefBase + 'behaviours/restrict-text/index.html'
+                },
+	            {
+	                name: 'Scrollable',
+	                href: sitemapHrefBase + 'behaviours/scrollable/index.html'
+	            }
+            ]
+        }, {
+            name: 'Menus',
+            href: sitemapHrefBase + 'menus/index.html',
+            nodes: [
+                {
+                    name: 'Accordion',
+                    href: sitemapHrefBase + 'menus/accordion/index.html'
+                },
+                {
+                    name: 'Breadcrumb',
+                    href: sitemapHrefBase + 'menus/breadcrumb/index.html'
+                },
+				{
+	                name: 'Context Menu',
+	                href: sitemapHrefBase + 'menus/context-menu/index.html'
+	            },
+	            {
+	                name: 'Menu',
+	                href: sitemapHrefBase + 'menus/menu/index.html'
+	            },
+                {
+                    name: 'Tabs',
+                    href: sitemapHrefBase + 'menus/tabs/index.html'
+                },
+                {
+                    name: 'Tabstrip',
+                    href: sitemapHrefBase + 'menus/tabstrip/index.html'
+                },
+                {
+                    name: 'Treeview',
+                    href: sitemapHrefBase + 'menus/treeview/index.html'
+                }
+            ]
+        }, {
+            name: 'Controls',
+            href: sitemapHrefBase + 'controls/index.html',
+            nodes: [
+				{
+                    name: 'Button',
+                    href: sitemapHrefBase + 'controls/button/index.html'
+                },
+                {
+                    name: 'Calendar',
+                    href: sitemapHrefBase + 'controls/calendar/index.html'
+                },
+				{
+                    name: 'ColorPicker',
+                    href: sitemapHrefBase + 'controls/colorpicker/index.html'
+                },
+				{
+                    name: 'Combobox',
+                    href: sitemapHrefBase + 'controls/combobox/index.html'
+                },
+                {
+                    name: 'DatePicker',
+                    href: sitemapHrefBase + 'controls/datepicker/index.html'
+                },
+				{
+                    name: 'Dialog',
+                    href: sitemapHrefBase + 'controls/dialog/index.html'
+                },
+				{
+                    name: 'Grid',
+                    href: sitemapHrefBase + 'controls/grid/index.html'
+                },
+                {
+                    name: 'Textbox',
+                    href: sitemapHrefBase + 'controls/textbox/index.html'
+                }
+            ]
+        }, {
+            name: 'Events',
+            href: sitemapHrefBase + 'events/index.html',
+            nodes: [
+                {
+                    name: 'Drag',
+                    href: sitemapHrefBase + 'events/drag/index.html'
+                },
+                {
+                    name: 'Mousewheel',
+                    href: sitemapHrefBase + 'events/mousewheel/index.html'
+                },
+                {
+                    name: 'Property Change',
+                    href: sitemapHrefBase + 'events/propchange/index.html'
+                },
+                {
+                    name: 'Resize',
+                    href: sitemapHrefBase + 'events/resize/index.html'
+                }
+            ]
+        }, {
+            name: 'Utilities',
+            href: sitemapHrefBase + 'utilities/index.html',
+            nodes: [
+                {
+                    name: 'Cookie',
+                    href: sitemapHrefBase + 'utilities/cookie/index.html'
+                },
+                {
+                    name: 'CSS Rule',
+                    href: sitemapHrefBase + 'utilities/css-rule/index.html'
+                },
+                {
+                    name: 'Helper Utilities',
+                    href: sitemapHrefBase + 'utilities/helpers/index.html',
+                    nodes: [
+                        {
+                            name: 'Edit Class',
+                            href: '#EditClass'
+                        },
+                        {
+                            name: 'Swap Class',
+                            href: '#SwapClass'
+                        },
+                        {
+                            name: 'Focus Class',
+                            href: '#FocusClass'
+                        },
+                        {
+                            name: 'Hover Class',
+                            href: '#HoverClass'
+                        },
+                        {
+                            name: 'Outer Html',
+                            href: '#OuterHtml'
+                        },
+                        {
+                            name: 'Is Child Of',
+                            href: '#IsChildOf'
+                        },
+                        {
+                            name: 'Prepend Zero',
+                            href: '#Prepend0'
+                        },
+                        {
+                            name: 'Random',
+                            href: '#Random'
+                        },
+                        {
+                            name: 'Trim',
+                            href: '#Trim'
+                        },
+                        {
+                            name: 'Key Value',
+                            href: '#KeyValue'
+                        },
+                        {
+                            name: 'Circular Shift',
+                            href: '#CircularShift'
+                        },
+                        {
+                            name: 'KeyValue',
+                            href: '#KeyValue'
+                        },
+                        {
+                            name: 'Return True',
+                            href: '#Return1'
+                        },
+                        {
+                            name: 'Return False',
+                            href: '#Return0'
+                        },
+                    ]
+                },
+                {
+                    name: 'Inheritance',
+                    href: sitemapHrefBase + 'utilities/inheritance/index.html'
+                },
+                {
+                    name: 'Multiview',
+                    href: sitemapHrefBase + 'utilities/multiview/index.html'
+                },
+                {
+                    name: 'Plugin',
+                    href: sitemapHrefBase + 'utilities/plugin/index.html',
+                    nodes: [
+                        {
+                            name: 'Pattern1',
+                            href: sitemapHrefBase + 'utilities/plugin/pattern1.html'
+                        },
+                        {
+                            name: 'Pattern2',
+                            href: sitemapHrefBase + 'utilities/plugin/pattern2.html'
+                        }
+                    ]
+                },
+				{
+                    name: 'Position',
+                    href: sitemapHrefBase + 'utilities/position/index.html'
+                },
+                {
+                    name: 'Scroll Parent',
+                    href: sitemapHrefBase + 'utilities/scroll-parent/index.html'
+                },
+                {
+                    name: 'Scroll To',
+                    href: sitemapHrefBase + 'utilities/scroll-to/index.html'
+                },
+                {
+                    name: 'Selectable',
+                    href: sitemapHrefBase + 'utilities/selectable/index.html'
+                },
+            ]
+        }]
+    }]
+}];
